8b1981f563e667a8c1f9d8ef9eecf7c863f7e89d
Tom Walpole <tom@tomwalpole.com>
2013-10-29 22:10:24 -0700

    use async in bulk loader

--- bin/bulk-load.js
+++ bin/bulk-load.js
@@ -3,18 +3,22 @@ var csv = require('csv');
 var redis = require('redis');
 var http = require('http');
 var https = require('https');
+var async = require('async');
 
 var argv = require('optimist')
 	.usage('\nImport a csv containing meta tag data for urls.\n\nUsage: $0 <file|"url">\n\n*note: if using a url - be sure to put quotes around it.')
-	.default('p', 6379)
+	.
+default ('p', 6379)
 	.describe('p', 'redis port')
 	.alias('p', 'port')
-	.default('h', 'localhost')
+	.
+default ('h', 'localhost')
 	.describe('h', 'redis host')
 	.alias('h', 'host')
-	.default('a','meta-tag')
-	.describe('a','prefix redis key to namespace your data')
-	.alias('a','prefix')
+	.
+default ('a', 'meta-tag')
+	.describe('a', 'prefix redis key to namespace your data')
+	.alias('a', 'prefix')
 	.demand(1)
 	.argv;
 
@@ -47,25 +51,32 @@ function readCsv(path, cb) {
 
 
 function parseCsv(stream) {
-	console.log('parse csv');
 	csv()
 		.from.stream(stream, {
 			delimiter: ',',
-			columns:true
+			columns: true
 		})
 		.to.array(function(data) {
 			/* TODO: support a header row */
 			for (var i = 0; i < data.length; i++) {
 				var row = data[i];
-				metaData = {};
-				Object.keys(row).forEach(function(k,i) {
-					var u = k.toUpperCase();
-					console.log(u +': ' + row[k]);
-					metaData[u] = row[k];
-				})
-				console.log(metaData);
-				client.hmset(argv.a + ':' + metaData['URL'], metaData);
+
+				async.eachSeries(Object.keys(row), function(k, cb) {
+						metaData = {};
+						var u = k.toUpperCase();
+						metaData[u] = row[k];
+						client.hmset(argv.a + ':' + metaData['URL'], metaData, function(err, result) {
+							cb(err);
+						});
+					},
+					function(err, result) {
+						if (err) {
+							console.log(err);
+						} else {
+							console.log('done.');
+						}
+						process.exit(0);
+					});
 			};
-			process.exit(0);
 		});
 }
